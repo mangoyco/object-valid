5e7b5a210aca69cf24408fb5c89ba22bc326f91e
ZR20030045-王彦超 <wangyanchao@zhongruigroup.com>
2020-06-19 15:49:28 +0800

    initgit

---
+++ valid.js
@@ -0,0 +1,64 @@
+class valid {
+  constructor(obj, rules = []) {
+    this.rules = []
+    this.initRules(rules)
+    this.formObj = obj
+  }
+
+  _isObj(obj) {
+    return Object.prototype.toString.call(obj) === '[object Object]'
+  }
+
+  _isRightRule(obj) {
+    return this._isObj(obj) && obj['field'] && obj['func'] && (typeof obj['func'] === 'function')
+  }
+
+  initRules(rules) {
+    if (Array.isArray(rules)) {
+      rules.forEach(rule => {
+        this.addRule(rule)
+      })
+    } else {
+      throw new Error('The second arg except an Array!')
+    }
+  }
+
+  addRule(obj) {
+    if (this._isRightRule(obj)) {
+      this.rules.push(obj)
+    } else {
+      console.warn('rule must be an Object with [name] & [func],and [func] must be a Function')
+    }
+  }
+
+  judgeFunc(all) {
+    let bol = true
+    let errmsg = []
+    for (let i = 0; i < this.rules.length; i++) {
+      let rule = this.rules[i]
+      let F = rule.func
+      if (!typeof F === 'function') {
+        throw new Error(`func Property Needs a Function Not a ${typeof F}`)
+      }
+      let r = F.call(this.formObj, this.formObj[rule.field])
+      if (!r) {
+        bol = false
+        errmsg.push(rule.emsg ? rule.emsg : `${rule.field} is not valid`)
+        if (!all) {
+          return {
+            isValid: bol,
+            errmsg: errmsg[0]
+          }
+        }
+      }
+    }
+    return {
+      isValid: bol,
+      errmsg: errmsg
+    }
+  }
+
+  isvalid(all) {
+    return this.judgeFunc(all)
+  }
+}
